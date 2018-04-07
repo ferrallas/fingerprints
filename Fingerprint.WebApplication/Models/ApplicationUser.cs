c903280478ace3e990251de2c6c7aded04708e37
Matteo Fabbri <matteo@phascode.org>
2018-04-07 10:12:20 +0200

    removed web app

--- Fingerprint.WebApplication/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using AspNetCore.Identity.Mongo;
-
-namespace Fingerprint.WebApplication.Models
-{
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : MongoIdentityUser
-    {
-    }
-}
